a14c3324b79dde4a697dbc42c83e5f3563799c7e
martin.blasko <martin.blasko@lifeliqe.com>
2025-01-03 12:56:59 +0100

    feat: implement animation frame handling and app state management

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use gloo_render::request_animation_frame;
+use gloo_render::AnimationFrame;
 use js_sys::Function;
 use ratatui::layout::Alignment;
 use ratatui::widgets::Block;
 use ratatui::widgets::Paragraph;
 use ratatui::Terminal;
 
+use std::cell::RefCell;
 use std::io::Result as IoResult;
+use std::rc::Rc;
 
 use wasm_bindgen::prelude::*;
 
@@ -15,16 +19,27 @@ mod utils;
 mod wasm_backend;
 use wasm_backend::WasmBackend;
 
+struct App {
+    count: u64,
+    some_text: String,
+}
+
+impl App {
+    fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+        window()
+            .unwrap()
+            .request_animation_frame(f.as_ref().unchecked_ref())
+            .unwrap();
+    }
+}
 // taken from https://github.com/rustwasm/gloo/blob/master/crates/render/src/lib.rs
 
-fn render() -> IoResult<()> {
-    let mut terminal = Terminal::new(WasmBackend::new()).unwrap();
-
+fn render(terminal: &mut Terminal<WasmBackend>, app_state: &mut App) -> IoResult<()> {
     terminal
         .draw(|f| {
             web_sys::console::log_1(&"Drawing before".into());
             f.render_widget(
-                Paragraph::new(f.count().to_string())
+                Paragraph::new(format!("Count: {}", app_state.count))
                     .alignment(Alignment::Center)
                     .block(Block::bordered()),
                 f.area(),
@@ -35,16 +50,30 @@ fn render() -> IoResult<()> {
 
     web_sys::console::log_1(&"Yo, yo, yo".into());
 
-    window()
-        .unwrap()
-        .request_animation_frame(&Function::from(Closure::once_into_js(move || {
-            // render().unwrap();
-        })))
-        .unwrap();
     Ok(())
 }
 
 fn main() {
-    render().unwrap();
+    let mut terminal = Terminal::new(WasmBackend::new()).unwrap();
+
+    let mut app_state = App {
+        count: 0,
+        some_text: "Hello World".to_string(),
+    };
+
+    let cb = Rc::new(RefCell::new(None));
+
+    *cb.borrow_mut() = Some(Closure::wrap(Box::new({
+        let cb = cb.clone();
+        move || {
+            // This should repeat every frame
+            app_state.count += 1;
+            // render(&mut terminal, &mut app_state).unwrap();
+            App::request_animation_frame(cb.borrow().as_ref().unwrap());
+        }
+    }) as Box<dyn FnMut()>));
+
+    App::request_animation_frame(cb.borrow().as_ref().unwrap());
+    // render(&mut terminal, &mut app_state).unwrap();
     web_sys::console::log_1(&"Done".into());
 }
