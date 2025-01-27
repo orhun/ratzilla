0e11661711e9bd9653b03678394d7a20586ff233
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-01-27 23:57:59 +0300

    feat(utils): add open_url function

--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,16 @@ pub fn set_document_title(title: &str) -> Result<(), Error> {
         .set_title(title);
     Ok(())
 }
+
+/// Open a URL in a new tab or the current tab.
+pub fn open_url(url: &str, new_tab: bool) -> Result<(), Error> {
+    let window = web_sys::window().ok_or(Error::UnableToRetrieveWindow)?;
+    if new_tab {
+        window.open_with_url(url)?;
+    } else {
+        let location = window.location();
+        location.set_href(url)?;
+        location.replace(url)?;
+    }
+    Ok(())
+}
