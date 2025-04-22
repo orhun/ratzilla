b18581d42973ea2c6d1ddc680c2310900e4b17a1
lurkny <larsonbm@colostate.edu>
2025-04-21 18:56:21 -0600

    feat(utils): add functions to call global JavaScript functions withor without context

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,9 @@ use crate::{
     error::Error,
 };
 
+use js_sys::{Array, Function, Reflect};
+use wasm_bindgen::{prelude::*, JsValue};
+
 /// Sets the document title.
 pub fn set_document_title(title: &str) -> Result<(), Error> {
     web_sys::window()
@@ -49,4 +52,132 @@ pub fn get_screen_size() -> Size {
     let (w, h) = get_raw_screen_size();
     // TODO: These are mildly magical numbers... make them more precise
     (w as u16 / 10, h as u16 / 19).into()
+}
+
+
+
+/// Calls a global JavaScript function by name, with a custom `this` context and an arbitrary number of arguments.
+///
+/// This function looks up the property `window[name]` on the global window, checks that it is a JavaScript
+/// function, and then calls it using `Function.prototype.apply`.
+///
+/// # Type Parameters
+///
+/// * `S: AsRef<str>` – A type that can be converted to a string slice; used for the name of the function.
+/// * `T: Into<JsValue>` – A type that can be converted into a `JsValue`, representing the `this` binding.
+/// * `I: IntoIterator` – An iterable collection of function arguments.
+/// * `I::Item: Into<JsValue>` – Each argument can be converted into a `JsValue`.
+///
+/// # Parameters
+///
+/// * `name` - The name of the JavaScript function (as a property on the global `window` object).
+/// * `this` - The value to use as the `this` binding during the function invocation.
+/// * `args` - An iterable list of arguments to pass to the function.
+///
+/// # Returns
+///
+/// * `Ok(JsValue)` with the result of the function call if successful.
+/// * `Err(Error)` if the window is not available, if the function is not found,
+///   or if the function call fails (e.g. due to a JavaScript exception).
+///
+/// # Examples
+///
+/// Calling a global JS function with a custom context:
+///
+/// ```no_run
+/// # use wasm_bindgen::JsValue;
+/// # use crate::utils::call_js_function_with_context;
+/// # use crate::error::Error;
+/// # fn example() -> Result<(), Error> {
+/// // Suppose `myObj` is a JS object you want to be the `this` value.
+/// let my_obj = JsValue::from(js_sys::Object::new());
+/// let result = call_js_function_with_context(
+///     "myJsFunction",  // JavaScript global function name
+///     my_obj,          // custom `this` context
+///     [JsValue::from(42), JsValue::from("hello")] // arguments
+/// )?;
+/// # Ok(())
+/// # }
+/// ```
+///
+/// Calling a function without caring about `this`:
+///
+/// ```no_run
+/// # use crate::utils::call_js_function_with_context;
+/// # use crate::error::Error;
+/// # fn example() -> Result<(), Error> {
+/// // This will set `this` to `null` which in non-strict mode defaults to the global object.
+/// let result = call_js_function_with_context("alert", JsValue::NULL, ["Hello from Rust"])?;
+/// # Ok(())
+/// # }
+/// ```
+pub fn call_js_function_with_context<S, I, T>(
+    name: S,
+    this: T,
+    args: I,
+) -> Result<JsValue, Error>
+where
+    S: AsRef<str>,
+    T: Into<JsValue>,
+    I: IntoIterator,
+    I::Item: Into<JsValue>,
+{
+    let window = web_sys::window().ok_or(Error::UnableToRetrieveWindow)?;
+    let func_val = Reflect::get(&window, &JsValue::from_str(name.as_ref()))
+        .map_err(Error::from)?;
+    let func = func_val
+        .dyn_into::<Function>()
+        .map_err(Error::from)?;
+    let param_array: Array = args.into_iter().map(Into::into).collect();
+    let ctx = this.into();
+    let result = func.apply(&ctx, &param_array).map_err(Error::from)?;
+    Ok(result)
+}
+
+/// Calls a global JavaScript function by name, defaulting the `this` context to `null`.
+///
+/// This is a convenience wrapper around [`call_js_function_with_context`]. It allows callers to
+/// simply provide the function name and an iterable of arguments without worrying about the `this`
+/// binding. Passing `null` as `this` means that in non‑strict mode, JavaScript will default to
+/// using the global `window` object.
+///
+/// # Type Parameters
+///
+/// * `S: AsRef<str>` – Type for representing the name of the function.
+/// * `I: IntoIterator` – An iterable collection of function arguments.
+/// * `I::Item: Into<JsValue>` – Each argument can be converted into a `JsValue`.
+///
+/// # Parameters
+///
+/// * `name` - The name of the JavaScript function (a property on the global `window` object).
+/// * `args` - An iterable list of arguments to pass to the function call.
+///
+/// # Returns
+///
+/// * `Ok(JsValue)` if the function is successfully called.
+/// * `Err(Error)` if the lookup or invocation fails.
+///
+/// # Examples
+///
+/// Calling a global function like `alert`:
+///
+/// ```no_run
+/// # use crate::utils::call_js_function;
+/// # use crate::error::Error;
+/// # fn example() -> Result<(), Error> {
+/// // Calls alert("Hello World!") on the global window.
+/// call_js_function("alert", ["Hello World!"])?;
+/// # Ok(())
+/// # }
+/// ```
+pub fn call_js_function<S, I>(
+    name: S,
+    args: I,
+) -> Result<JsValue, Error>
+where
+    S: AsRef<str>,
+    I: IntoIterator,
+    I::Item: Into<JsValue>,
+{
+    call_js_function_with_context(name, JsValue::NULL, args)
 }
