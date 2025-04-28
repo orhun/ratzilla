a07c68b96be30fe0a8b8a30dec0927c75bd3bdf0
Adrian Papari <a@onedaybeard.net>
2025-04-28 22:22:03 +0200

    feat(canvas): Extend CanvasBackend with options (#88)
    * extend CanvasBackend with options
    
    - added CanvasBackendOptions; exposes grid_id and size
    - new example: canvas-waves
    
    * fix formatting
    
    * feat: Update examples/canvas_waves/index.html
    
    ---------
    
    Co-authored-by: Orhun Parmaksız <orhunparmaksiz@gmail.com>

---
+++ examples/canvas_waves/src/main.rs
@@ -0,0 +1,28 @@
+// This example shows the full range of RGB colors that can be displayed in the browser.
+
+mod wave_effect;
+
+use ratzilla::{
+    backend::canvas::CanvasBackendOptions, ratatui::Terminal, CanvasBackend, WebRenderer,
+};
+use tachyonfx::{EffectRenderer, IntoEffect};
+use wave_effect::WaveInterference;
+
+fn main() -> std::io::Result<()> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let backend =
+        CanvasBackend::new_with_options(CanvasBackendOptions::new().grid_id("container"))?;
+    let terminal = Terminal::new(backend)?;
+
+    let mut effect = WaveInterference::new().into_effect();
+    let mut last_tick = web_time::Instant::now();
+
+    terminal.draw_web(move |frame| {
+        let now = web_time::Instant::now();
+        let elapsed = now.duration_since(last_tick);
+        last_tick = now;
+
+        frame.render_effect(&mut effect, frame.area(), elapsed.into());
+    });
+    Ok(())
+}
